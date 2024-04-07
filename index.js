73ab5ff4cb176e732ff45c9f204cef75793529d1
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-04-07 22:19:44 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let element = document.querySelector("main");
+element.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Mercy is the champion";
+document.body.append(newHeader);
